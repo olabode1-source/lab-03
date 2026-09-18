d6715292f1f1246ab91fc43764b7ea51f194615f
olabode1-source <olabode1@ualberta.ca>
2026-09-18 16:58:10 -0600

    Update MainActivity.kt

--- code/app/src/main/java/com/example/listycity3/MainActivity.kt
+++ code/app/src/main/java/com/example/listycity3/MainActivity.kt
@@ -4,20 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.listycity3.ui.theme.ListyCity3Theme
 
 class MainActivity : ComponentActivity() {
@@ -27,9 +21,17 @@ class MainActivity : ComponentActivity() {
         val cityRepository = CityRepository()
         setContent {
             ListyCity3Theme {
+                var citiesList by remember { mutableStateOf(cityRepository.cities) }
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     CityListScreen(
-                        cities = cityRepository.cities,
+                        cities = citiesList,
+                        onAddCity = { newCity ->
+                            citiesList = citiesList + newCity
+                        },
+                        onModifyCity = { index, updatedCity ->
+                            citiesList = citiesList.toMutableList().apply { this[index] = updatedCity }
+                        },
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
